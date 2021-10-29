f71c5c385869565fce4cafcae919af75e671dad1
David <ludawei789@126.com>
2021-10-29 17:38:24 +0800

    初始化

---
+++ src/views/Home.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="home">
+    <el-container>
+      <el-aside width="260px">Aside</el-aside>
+      <el-container>
+        <el-header>
+          <span>个人简介</span>
+        </el-header>
+        <el-main>Main</el-main>
+        <el-footer>Footer</el-footer>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  // components: {
+  //   HelloWorld,
+  // },
+};
+</script>
+<style lang="scss" scoped>
+.home {
+  width: 100%;
+  height: 100%;
+  .el-container {
+    height: 100%;
+    .el-header {
+      background-color: #fff;
+    }
+  }
+}
+
+// Ui样式
+.el-header,
+.el-footer {
+  background-color: #b3c0d1;
+  color: var(--el-text-color-primary);
+  text-align: center;
+  line-height: 60px;
+}
+
+.el-aside {
+  background-color: #d3dce6;
+  color: var(--el-text-color-primary);
+  text-align: center;
+  line-height: 200px;
+}
+
+.el-main {
+  background-color: #e9eef3;
+  color: var(--el-text-color-primary);
+  text-align: center;
+  line-height: 160px;
+}
+
+body > .el-container {
+  margin-bottom: 40px;
+}
+
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+  line-height: 260px;
+}
+
+.el-container:nth-child(7) .el-aside {
+  line-height: 320px;
+}
+</style>
